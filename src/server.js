88a98a7a7e294c8488d041d0ee4853557167b2a2
nguyenat2020 <great.expectations7760@gmail.com>
2023-08-29 23:51:42 +0900

    ORM sequelize

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 import bodyParser from "body-parser";
 require("dotenv").config();
+import connection from "../config/connectDB";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,9 @@ configViewEngine(app);
 // config body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// test connection db
+connection();
 
 // init web route
 initWebRoutes(app);
